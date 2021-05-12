d57b14a4970f28cef77577e2f1194827637438ab
j.zhang <j.zhang@sunmedia.com.cn>
2021-05-11 19:35:32 -0700

    add the ardunio files

---
+++ cores/arduino/board.h
@@ -0,0 +1,18 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+/*
+ * Core and peripherals registers definitions
+*/
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+void init(void) ;
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* _BOARD_H_ */
+
---
+++ system/sp7021/system_sp7021.c
@@ -0,0 +1,16 @@
+#include "sp70xx.h"
+
+
+uint32_t SystemCoreClock = HSI_VALUE;
+
+
+void SystemInit (void)
+{
+  /*system  init*/
+
+
+}
+
+
+
+
