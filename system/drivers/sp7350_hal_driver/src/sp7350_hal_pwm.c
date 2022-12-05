3db66394a5193dd9fbd01ff482ccfdae25a0ff51
haiq.tang <haiq.tang@sunmedia.com.cn>
2022-12-05 15:35:11 +0800

    fixed for comiple error

--- system/drivers/sp7350_hal_driver/src/sp7350_hal_pwm.c
+++ system/drivers/sp7350_hal_driver/src/sp7350_hal_pwm.c
@@ -1,23 +1,33 @@
 #include "sp7350_hal_pwm.h"
 #define PWM_SRC_CLK         (200*1000*1000)
 
-static uint32_t g_resolution_sel[PWM_MAX]={0};
+static uint32_t g_resolution_sel[PWM_MAX]={0}; // for freq set
 
-/*frequency = (1 / sys clk) * (1~262142)PWM DD0(18bit) * 4096 */
-static int __PWM_Set_by_freq(int pwm_num,uint32_t freq,uint32_t duty_cycle)
+
+/*
+256: 3 ~ 781k
+4096: 0.2 ~ 49k
+*/
+static int _PWM_Set(int pwm_num,uint32_t period,uint32_t duty)
 {
-	uint32_t pwm_dd;
-
-	if(g_resolution_sel[pwm_num] == PWM_RESOLUTION_1_256 && duty_cycle >= RESOLUTION_256)
+	uint64_t duty_temp;
+	uint32_t pwm_dd,pwm_resolution;
+	
+	if(duty == 0 || duty >= period)
+		return HAL_ERROR;
+	
+	if(g_resolution_sel[pwm_num] == PWM_RESOLUTION_1_256 )
 	{
-		duty_cycle = (RESOLUTION_256-1);
+		pwm_resolution = RESOLUTION_256;
 	}
-	else if(duty_cycle >= RESOLUTION_4096)
+	else
 	{
-		duty_cycle = (RESOLUTION_4096-1);
+		pwm_resolution = RESOLUTION_4096;
 	}
-
-	pwm_dd = PWM_SRC_CLK / freq / HAL_PWM_Get_Resolution_By_Num(pwm_num);
+	duty_temp = ((uint64_t)duty*pwm_resolution)/period;
+	
+	/* pwm_dd = (1 / (sys clk * frequency)) * 4096 */
+	pwm_dd = ((uint64_t)period * PWM_SRC_CLK )/ (1000*1000*1000) / pwm_resolution;
 
 	if(pwm_dd == 0)
 	{
@@ -30,7 +40,7 @@ static int __PWM_Set_by_freq(int pwm_num,uint32_t freq,uint32_t duty_cycle)
 	}
 
 	/* set duty cycle */
-	PWM_REG->pwm_chx_cfg[pwm_num].pwm_duty = duty_cycle;
+	PWM_REG->pwm_chx_cfg[pwm_num].pwm_duty = duty_temp;
 
 	return HAL_OK;
 }
@@ -39,24 +49,21 @@ int HAL_PWM_INIT(PWM_InitTypeDef *PWM_Init)
 {
 	int pwm_num;
 	
-	assert_param(PWM_Init);
-	assert_param(IS_PWM_NUM_VALID(pwm_num));
-	assert_param(IS_PWM_DUTY_VALID(PWM_Init->pwm_duty));
-	assert_param(IS_PWM_RESOLUTION_SEL_VALID(PWM_Init->pwm_resolution_sel));
+	if(!PWM_Init || !IS_PWM_NUM_VALID(PWM_Init->pwm_num) || !IS_PWM_RESOLUTION_SEL_VALID(PWM_Init->resolution_sel))
+	{
+		return HAL_ERROR;
+	}
 
 	pwm_num = PWM_Init->pwm_num;
-	g_resolution_sel[pwm_num] = PWM_Init->pwm_resolution_sel;
-	
-	/* set pwm pinmux */
-	HAL_PINMUX_Cfg(PINMUX_DISP_PWM,1);
+	g_resolution_sel[pwm_num] = PWM_Init->resolution_sel;
 	
 	/* select pwm resolution  256/4095 */
-	MODIFY_REG(PWM_REG->pwm_resolution,1<<(pwm_num),PWM_Init->pwm_resolution_sel<<(pwm_num));
+	MODIFY_REG(PWM_REG->pwm_resolution,1<<(pwm_num),PWM_Init->resolution_sel<<(pwm_num));
 
 	/* select PWM_DD, PWM_NUM_X match PWM_DD_X */
 	PWM_REG->pwm_chx_cfg[pwm_num].pwm_du_dd_sel = pwm_num + ePWM_DD0;
 
-	return __PWM_Set_by_freq(pwm_num,PWM_Init->pwm_freq,PWM_Init->pwm_duty);
+	return _PWM_Set(pwm_num,PWM_Init->period_ns,PWM_Init->duty_ns);
 }
 
 
@@ -74,20 +81,8 @@ void HAL_PWM_Stop(int pwm_num)
 	PWM_REG->pwm_en &= ~(1 << pwm_num);     // set pwm disable
 }
 
-int HAL_PWM_Get_Resolution_By_Num(int pwm_num)
+void HAL_PWM_Period_Set(int pwm_num,uint32_t period,uint32_t duty)
 {
-	uint32_t pwm_resolution;
-
-	if(g_resolution_sel[pwm_num] == PWM_RESOLUTION_1_256)
-		pwm_resolution = RESOLUTION_256;
-	else
-		pwm_resolution = RESOLUTION_4096;
-
-	return pwm_resolution;
-}
-
-void HAL_PWM_Freq_Set(int pwm_num,uint32_t freq,uint32_t duty_cycle)
-{
-	__PWM_Set_by_freq(pwm_num,freq,duty_cycle);
+	_PWM_Set(pwm_num,period,duty);
 }
 
